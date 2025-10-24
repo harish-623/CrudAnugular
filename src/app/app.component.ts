871185d967b0c46bd55d51ccdca089887aa94cea
harish-623 <138812943+harish-623@users.noreply.github.com>
2025-10-24 14:32:15 +0530

    Updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
 
   constructor(private router: Router) {}
   showProfile = false;
-userName = 'Harish';
+userName = 'UdayKumar';
 
   goToProfile() {
   this.router.navigate(['/profile']);
